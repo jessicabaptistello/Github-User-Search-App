7c4acb9ce75ac4dd1b54493dc1ea3bd18506382e
Jessica Baptistello <jessica.a.baptistello@gmail.com>
2026-02-09 15:59:03 +0000

    Atualizaçao de layout

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 1) Pegamos os elementos do HTML
 const input = document.querySelector("#usernameInput");
 const button = document.querySelector("#searchBtn");
 
@@ -11,21 +10,20 @@ const bioEl = document.querySelector("#bio");
 const reposEl = document.querySelector("#repos");
 const profileLink = document.querySelector("#profileLink");
 
-// 2) Função utilitária para mostrar mensagens
+
 function setMessage(text, isError = false) {
   message.textContent = text;
   message.style.color = isError ? "crimson" : "#333";
 }
 
-// 3) Função utilitária para esconder/mostrar o card
 function showResult(show) {
   if (show) result.classList.remove("hidden");
   else result.classList.add("hidden");
 }
 
-// 4) Função principal: busca o usuário na API do GitHub
+
 async function fetchGitHubUser(username) {
-  // Limpamos estado anterior
+
   setMessage("Carregando...");
   showResult(false);
 
@@ -33,28 +31,28 @@ async function fetchGitHubUser(username) {
     const url = `https://api.github.com/users/${username}`;
     const response = await fetch(url);
 
-    // AQUI entra o tratamento do 404
+ 
     if (response.status === 404) {
       setMessage("Usuário não encontrado (404).", true);
       return;
     }
 
-    // Se não for OK (ex: 403 rate limit, 500 etc.)
+
     if (!response.ok) {
       setMessage(`Erro na requisição: ${response.status}`, true);
       return;
     }
 
-    // Transformar resposta em JSON (objeto JS)
+
     const data = await response.json();
 
-    // 5) Colocar os dados no DOM
+
     avatar.src = data.avatar_url;
     avatar.alt = `Foto de ${data.login}`;
 
     nameEl.textContent = data.name ? data.name : data.login;
 
-    // bio pode ser null
+
     bioEl.textContent = data.bio ? data.bio : "Sem bio disponível.";
 
     reposEl.textContent = data.public_repos;
@@ -62,15 +60,15 @@ async function fetchGitHubUser(username) {
     profileLink.href = data.html_url;
     profileLink.textContent = "Ver perfil no GitHub";
 
-    setMessage(""); // limpa mensagem
-    showResult(true); // mostra o card
+    setMessage(""); 
+    showResult(true); 
   } catch (error) {
-    // Erros de rede, internet, etc.
+
     setMessage("Erro de rede. Tente novamente.", true);
   }
 }
 
-// 6) Clique no botão
+
 button.addEventListener("click", () => {
   const username = input.value.trim();
 
@@ -83,7 +81,6 @@ button.addEventListener("click", () => {
   fetchGitHubUser(username);
 });
 
-// (Opcional) Buscar apertando Enter
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     button.click();
